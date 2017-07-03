50d4eb246da23cdd278660a3ba3a8f498fcfc3b4
Edgar Khanzadian <edgar.khanzadyan@yandex.ru>
2017-07-03 15:28:46 +0400

    optimized merge sort

--- src/algos/merge_sort.ml
+++ src/algos/merge_sort.ml
@@ -2,15 +2,15 @@ let merge_sort arr =
   let rec merge arr1 arr2 acc =
     match arr1, arr2 with
     | (h1 :: t1 as a1), (h2 :: t2 as a2) ->
-      if h1 >= h2 then merge t1 a2 (acc@[h1])
-      else merge a1 t2 (acc@[h2])
-    | [], h2 :: t2 -> merge [] t2 (acc@[h2])
-    | h1 :: t1, [] -> merge t1 [] (acc@[h1])
-    | [], [] -> acc in
+      if h1 >= h2 then merge t1 a2 (h1 :: acc)
+      else merge a1 t2 (h2 :: acc)
+    | [], h2 :: t2 -> merge [] t2 (h2 :: acc)
+    | h1 :: t1, [] -> merge t1 [] (h1 :: acc)
+    | [], [] -> List.rev acc in
   let rec divide k acc = function
     | hd :: tl ->
-      if k <= 1 then (acc@[hd], tl)
-      else divide (k-1) (acc@[hd]) tl
+      if k <= 1 then (hd :: acc, tl)
+      else divide (k-1) (hd :: acc) tl
     | [] -> (acc, []) in
   let rec aux l =
     let len = List.length l in
