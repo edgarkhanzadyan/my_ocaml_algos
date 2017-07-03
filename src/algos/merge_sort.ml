fd46e6eaf72054d86233b8782921c3b72ffb65de
Edgar Khanzadian <edgar.khanzadyan@yandex.ru>
2017-07-03 23:00:41 +0400

    merge sort for array

--- src/algos/merge_sort.ml
+++ src/algos/merge_sort.ml
@@ -20,3 +20,42 @@ let merge_sort arr =
     else
       l in
   aux arr
+
+let merge_sort_array arr =
+  let rec merge arr1 arr2 acc k1 k2 k =
+    if k >= Array.length acc then acc
+    else (
+      let l1 = Array.length arr1 in
+      let l2 = Array.length arr2 in
+      if k1 < l1 && k2 < l2 then (
+        if arr1.(k1) > arr2.(k2) then (
+          acc.(k) <- arr1.(k1);
+          merge arr1 arr2 acc (k1 + 1) k2 (k + 1) 
+        )
+        else (
+          acc.(k) <- arr2.(k2);
+          merge arr1 arr2 acc k1 (k2 + 1) (k + 1)
+        )
+      )
+      else if k2 >= l2 then (
+        acc.(k) <- arr1.(k1);
+        merge arr1 arr2 acc (k1 + 1) k2 (k + 1)
+      )
+      else if k1 >= l1 then (
+        acc.(k) <- arr2.(k2);
+        merge arr1 arr2 acc k1 (k2 + 1) (k + 1)
+      )
+      else acc
+    ) in
+  let rec main arr =
+    let len = Array.length arr in
+    if len > 1 then (
+      let a1 = Array.sub arr 0 (len / 2) in
+      let a2 = Array.sub arr (len / 2) (if len mod 2 = 1
+                                        then len / 2 + 1
+                                        else len / 2) in
+      let new_arr = Array.make len 0 in
+      merge (main a1) (main a2) new_arr 0 0 0
+    )
+    else arr in
+  main arr
--- src/main.ml
+++ src/main.ml
@@ -1,20 +1,38 @@
-let time f x =
-    let t = Sys.time() in
-    let fx = f x in
-    Printf.printf "Execution time: %fs\n" (Sys.time() -. t);
-    fx;;
+let rand_int = 1073741823;;
+let random_vect length =
+  let t = Array.make length 0 in
+  for pos = 0 to length - 1 do
+    t.(pos) <- Random.int rand_int
+  done;
+  t;;
 
-let f n =
-  print_endline "";
-  print_int n;;
+let rec random_list = function
+  | 0 -> []
+  | n -> (Random.int rand_int) :: (random_list (n - 1));;
 
-let range fir sec =
-  let rec aux a b acc =
-    if a > b then aux (a-1) b (acc @ [a])
-    else if a < b then aux (a+1) b (acc @ [a])
-    else acc @ [a] in
-  aux fir sec [];;
+let time randomize sort length =
+  let data = randomize length in
+  let start = Sys.time() in
+  sort data;
+  Sys.time () -. start;;
 
-let ran = range 0 10000;;
-let res = time Merge_sort.merge_sort ran;;
-List.iter f res;;
+let time_fn name random fn counts =
+  let rec aux = function
+    | [] -> ()
+    | count :: tail ->
+      Printf.printf "%s : %d elements -> %f\n"
+        name count (time random fn count);
+      aux tail in
+  aux counts;
+  print_newline ();;
+time_fn
+  "merge_sort"
+  random_vect
+  Merge_sort.merge_sort_array
+  [10; 100; 1000; 10000; 100000; 1000000];
+(* merge_sort : 10 elements -> 0.000009 *)
+(* merge_sort : 100 elements -> 0.000078 *)
+(* merge_sort : 1000 elements -> 0.001057 *)
+(* merge_sort : 10000 elements -> 0.013286 *)
+(* merge_sort : 100000 elements -> 0.136587 *)
+(* merge_sort : 1000000 elements -> 1.578245 *)
